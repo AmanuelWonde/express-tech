38635fb76831296dd9543cd2d4eae879e564276c
Abel <mulatabel0@gmail.com>
2024-03-04 11:32:42 +0500

    fixed navbar bug

--- express-tech-website/src/components/Navbar.jsx
+++ express-tech-website/src/components/Navbar.jsx
@@ -12,9 +12,16 @@ export function Navbar(){
     show === false ? setShow(true) : setShow(false);
   }
 
+  function handleClick(){
+    if(show == true){
+      setShow(false);
+      setName('menu');
+    }
+  }
+
   return(
     <div className="bg-black  sticky z-20">
-      <nav className="bg-black h-[80px] px-4 text-white flex flex-row-reverse md:flex-row justify-between items-center 2xl:w-[1280px] mx-auto ">
+      <nav className="bg-black h-[80px] px-4 text-white flex flex-row-reverse md:flex-row justify-between items-center 2xl:w-[1280px] mx-auto">
         <div className="text-3xl  cursor-pointer lg:hidden pt-2 z-10">
           <ion-icon name={name} onClick={onToggleMenu}></ion-icon>
         </div>
@@ -23,13 +30,13 @@ export function Navbar(){
         </div>
         <div>
           <ul className={`lg:static absolute left-0 bg-black lg:min-h-fit min-w-full flex flex-col lg:flex-row lg:justify-center px-3 lg:px-0 ${show ? "top-[100%]  min-h-[35vh]": "top-[-400%]"} transition-all duration-500 lg:items-center lg:z-0 gap-6 z-[-1]`}>
-            <NavLink to="/" className="navLink">Home</NavLink>
-            <NavLink to="aboutUs" className="navLink">About Us</NavLink>
-            <NavLink to="team" className="navLink">Team</NavLink>
-            <NavLink to="portfolio" className="navLink">Portfolio</NavLink>
-            <NavLink to="services" className="navLink">Services</NavLink>
-            <NavLink to="projectBid" className="navLink">Project Bid</NavLink>
-            <NavLink to="blog" className="navLink">Blog</NavLink>
+            <NavLink onClick={handleClick} to="/" className="navLink">Home</NavLink>
+            <NavLink onClick={handleClick} to="aboutUs" className="navLink">About Us</NavLink>
+            <NavLink onClick={handleClick} to="team" className="navLink">Team</NavLink>
+            <NavLink onClick={handleClick} to="portfolio" className="navLink">Portfolio</NavLink>
+            <NavLink onClick={handleClick} to="services" className="navLink">Services</NavLink>
+            <NavLink onClick={handleClick} to="projectBid" className="navLink">Project Bid</NavLink>
+            <NavLink onClick={handleClick} to="blog" className="navLink">Blog</NavLink>
           </ul>
         </div>
         <div className="hidden md:block z-10  ">
